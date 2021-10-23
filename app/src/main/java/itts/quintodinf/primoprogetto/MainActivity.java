ccab011e7876659e24662a29e6a5911dfa03acfb
Fred <ficofedebla@gmail.com>
2021-10-23 12:47:28 +0200

    Play button added and made it work properly.

--- app/src/main/java/itts/quintodinf/primoprogetto/MainActivity.java
+++ app/src/main/java/itts/quintodinf/primoprogetto/MainActivity.java
@@ -2,6 +2,7 @@ package itts.quintodinf.primoprogetto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -9,7 +10,7 @@ import android.widget.*;
 public class MainActivity extends AppCompatActivity {
 
     EditText editTxt, editTxt2;
-    Button resButton, testButton;
+    Button resButton, testButton, playBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +18,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTxt = findViewById(R.id.editTxt);
-        resButton = findViewById(R.id.resButton);
+        resButton = findViewById(R.id.resBtn);
         editTxt2 = findViewById(R.id.editTxt2);
-        testButton = findViewById(R.id.testButton);
-
-        resButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editTxt.getText().clear();
-            }
-        });
-
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View c) {
-                editTxt2.setText(editTxt.getText());
-            }
-        });
-
+        testButton = findViewById(R.id.testBtn);
+        playBtn = findViewById(R.id.playBtn);
     }
+
+    public void resBtn(View v) {
+        editTxt.getText().clear();
+    }
+
+    public void testBtn(View v) {
+        editTxt2.setText(editTxt.getText());
+    }
+
+    public void playBtn(View v) {
+        Intent i = new Intent(this, SecondActivity.class);
+        i.putExtra("nome", editTxt.getText());
+        startActivity(i);
+    }
+
 }
